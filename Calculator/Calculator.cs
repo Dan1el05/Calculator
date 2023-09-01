d68a499b8a56f9f2f977ff093cd8521af3673d13
dan1el <dan1e@DANIEL>
2023-09-01 17:01:32 +0200

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+        private int lastResult;
+
+        public static int Addition(int a, int b)
+        {
+            int lastResult = a + b;
+            return lastResult;
+        }
+
+        public static int Subtraktion(int a, int b)
+        {
+            int lastResult = a - b;
+            return lastResult;
+        }
+
+        public static int Multiplikation(int a, int b)
+        {
+            int lastResult = a * b;
+            return lastResult;
+        }
+
+        public static int Division(int a, int b)
+        {
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine("Die Eingabe ist null. Bitte gebe eine Ganza zahl ein: ");
+            }
+
+            int lastResult = a / b;
+            return lastResult;
+        }
+
+        public static int ReadInt(string promt)
+        {
+            Console.WriteLine(promt);
+            int number;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, o33ut number) && number > 0)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine positive ganze Zahl ein.");
+                }
+            }
+        }
+
+        public static void Schow( int a, int b, int lastResult)
+        {
+            Console.WriteLine($"Das Ergebnis von {a} und {b} ist {lastResult}");
+        }
+
+
+        public int GetLastResult()
+        {
+            return lastResult;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Calculator calculator = new Calculator();
+
+            int a = Calculator.ReadInt("Geben sie eine Zahl ein: ");
+          int b = Calculator.ReadInt("Geben sie eine Zahl ein ");
+          int sum = Calculator.Addition(a, b);
+          Calculator.Schow(a, b, sum);
+
+
+
+
+         int c = Calculator.ReadInt("Geben sie eine Zahl ein ");
+         int d = Calculator.ReadInt("Geben sie eine Zahl ein ");
+         int sumSub = Calculator.Subtraktion(c, d);
+         Calculator.Schow( c, d, sumSub);
+                
+
+            
+                
+         int e = Calculator.ReadInt("Geben sie eine Zahl ein: ");
+         int f = Calculator.ReadInt("Geben sie eine Zahl ein ");
+         int sumMu = Calculator.Multiplikation(e, f);
+         Calculator.Schow(e, f, sumMu);
+                
+
+                
+         int g = Calculator.ReadInt("Geben sie eine Zahl ein: ");
+         int h = Calculator.ReadInt("Geben sie eine Zahl ein ");
+         int sumDiv = Calculator.Division(g, h);
+         Calculator.Schow(g, h, sumDiv);
+            int lastResult = calculator.GetLastResult();
+            Console.WriteLine("Das letzte resultat ist" + lastResult);
+
+        }
+    }
+ }
